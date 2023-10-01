6b2ddb1fe2ca9a992ff1fac8041c6ed97b666d53
shyam-lal <slal4252@gmail.com>
2023-10-02 02:25:47 +0530

    changes.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,8 +8,8 @@ import Foundation
 import firebase_core
 import firebase_database
 import package_info_plus_macos
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
